a967bed5f356ab6bd81d31b5a972020b1762ba0e
yoonseooo71 <yoonseooo71@gmail.com>
2023-05-04 14:07:29 +0900

    theme color setting

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,6 +1,14 @@
 const theme = {
   colors : {
-
+    background : "black",
+    pianoBackground : "white",
+    C : "#FF0000",
+    D : "#FFA500", 
+    E : "#FFFF00",
+    F : "#008000",
+    G : "#0000FF",
+    A : "#00008B",
+    B : "#800080"
   }
 } ; 
 export default theme ; 
