4b3aeddef34a16d9ca3e4c9b2fa054c34a8ac1a3
yoonseooo71 <yoonseooo71@gmail.com>
2023-05-09 04:03:52 +0900

    remove animation end element

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import styled from "styled-components";
 import sounds from "./audio/sound";
+import { v4 as uuidv4 } from 'uuid';
 import { useState } from "react";
 import { useRef } from "react";
 import { useEffect } from "react";
 function App() {
   const [moveKeys,setMoveKeys] = useState([]); 
+  const moveKeysRef = useRef([]);  
+  const setMoveKeysRef = (data)=>{
+    moveKeysRef.current = data ; 
+    setMoveKeys(data) ; 
+  }
   const changeKey = { //키별 계이름 
     's':'C',
     'd':'D',
@@ -30,13 +36,16 @@ function App() {
     audio.volume=0.5; //소리크기 조절 
     audio.play()
   }
+  const animationEndHandler = (event) => {
+    const filterMoveKeys = moveKeysRef.current.filter((element)=>element.props.id !== event.target.id); 
+    setMoveKeysRef(filterMoveKeys);
+  }
   const keyDownHandler = (e)=>{ //키입력시 피아노소리 출력
     if (e.repeat) return; //키누르는중 중복 이벤트 발생
     else if (e.key in changeKey) { //입력한 키가 사용할키인지 확인
       playSound(sounds[changeKey[e.key]]);
       const clientRect = keysDom[changeKey[e.key]].current.getBoundingClientRect(); //요소위치값 가져오기
-      setMoveKeys(moveKeys=> [...moveKeys,<MovePianoKey key={moveKeys.length} type={changeKey[e.key]} top={clientRect.top} left={clientRect.left}/>])// 이동하는 에니메이션 요소 추가
-      console.log(moveKeys);
+      setMoveKeysRef([...moveKeysRef.current,<MovePianoKey id={uuidv4()} key={uuidv4()} type={changeKey[e.key]} top={clientRect.top} left={clientRect.left} onAnimationEnd={animationEndHandler}/>])// 이동하는 에니메이션 요소 추가
     }
   }
   useEffect(()=>{ //이벤트가 발생하면 state를 사용해 랜더링이되며 이벤트가 재생성되서 그걸 막기위해 useEffect 사용
